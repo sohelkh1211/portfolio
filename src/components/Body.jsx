860c74cf508ecec8b4d62fc9ffcea5eadb90ac40
sohelkh1211 <khansohailabdussalam207@gmail.com>
2024-01-29 00:22:13 +0530

    Added motion to Navbar elements

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,40 +1,50 @@
 import { useState, useEffect } from "react";
 import ML from "../assets/ml.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 const Body = () => {
   return (
     <div className="absolute items-center flex flex-row flex-wrap sm:flex sm:mx-auto max-w-4xl lg:left-60 md:left-[110px] sm:left-[100px] xs:left-[60px] gap-4 top-[130px] w-auto">
-      <div className="flex flex-col items-center justify-center">
+      <motion.div className="flex flex-col items-center justify-center" variants={fadeIn("right", "spring", 0.2, 1.5)}
+        initial="hidden"
+        animate="show">
         <div className="h-4 w-4 rounded-full fun-gradient1" />
         <div className="violet-gradient h-8 w-1" />
-      </div>
-      <h1 className="text-[50px] fun-gradient">Sohel</h1>
-      <div className="container flex flex-wrap xs:flex-wrap xs:flex">
+      </motion.div>
+      <motion.div variants={fadeIn("left", "spring", 0.2, 1.5)}
+        initial="hidden"
+        animate="show">
+        <h1 className="text-[50px] fun-gradient">Sohel</h1>
+      </motion.div>
+      <motion.div className="container flex flex-wrap xs:flex-wrap xs:flex" variants={fadeIn("up", "spring", 0.4, 2)}
+        initial="hidden"
+        animate="show">
         <p className="text-[16px] fitness-about sm:max-w-[520px] xs:max-w-[245px] flex-row">
           Currently pursuing degree in AI & ML Engineering. Have interest in web
           development, Machine Learning
           & Data Analysis. I'm always eager to learn new technologies.
         </p>
-      </div>
-        <div className="container flex-col sm:top-56 xs:top-[275px] c-scroll-icon scroll-smooth" onClick={() => window.scrollTo(0,400)}>
-          <div className="c-scroll-icon-line-mask">
-            <div className="c-scroll-icon-line"></div>
+      </motion.div>
+      <div className="container flex-col sm:top-56 xs:top-[275px] c-scroll-icon scroll-smooth" onClick={() => window.scrollTo(0, 400)}>
+        <div className="c-scroll-icon-line-mask">
+          <div className="c-scroll-icon-line"></div>
+        </div>
+        <div className="c-scroll-icon-triangle">
+          <div className="c-scroll-icon-triangle-mask first">
+            <div className="c-scroll-icon-triangle-line first"></div>
           </div>
-          <div className="c-scroll-icon-triangle">
-            <div className="c-scroll-icon-triangle-mask first">
-              <div className="c-scroll-icon-triangle-line first"></div>
-            </div>
-            <div className="c-scroll-icon-triangle-mask right">
-              <div className="c-scroll-icon-triangle-line right"></div>
-            </div>
-            <div className="c-scroll-icon-triangle-mask left">
-              <div className="c-scroll-icon-triangle-line left"></div>
-            </div>
-            <div className="c-scroll-icon-triangle-mask last">
-              <div className="c-scroll-icon-triangle-line last"></div>
-            </div>
+          <div className="c-scroll-icon-triangle-mask right">
+            <div className="c-scroll-icon-triangle-line right"></div>
+          </div>
+          <div className="c-scroll-icon-triangle-mask left">
+            <div className="c-scroll-icon-triangle-line left"></div>
+          </div>
+          <div className="c-scroll-icon-triangle-mask last">
+            <div className="c-scroll-icon-triangle-line last"></div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,93 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { navLinks } from "../constants";
-import Resume from '../assets/Resume.pdf'
+import Resume from '../assets/Resume.pdf';
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 import { TabTitle } from "../constants";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
-      <nav className="flex items-center z-20 fixed top-5 px-6 py-5 w-full mx-auto max-w-6xl">
-        <div className="flex border shadow-2xl lg:right-60 lg:left-60 justify-between rounded-xl mx-auto lg:md:sm:w-[550px] items-center fixed px-6 py-3 md:space-x-14 lg:space-x-16 xl:space-x-20 md:left-30 md:right-30 sm:left-20 sm:right-20 xs:left-10 xs:right-10 xs:px-6 bg-slate-50">
-          <Link
-            to="/"
-            className="flex items-center"
-            onClick={() => {
-              window.scrollTo(0, 0);
-              setActive("");
-            }}
+    <nav className="flex items-center z-20 fixed top-5 px-6 py-5 w-full mx-auto max-w-6xl">
+      <motion.div className="flex border shadow-2xl lg:right-60 lg:left-60 justify-between rounded-xl mx-auto lg:md:sm:w-[550px] items-center fixed px-6 py-3 md:space-x-14 lg:space-x-16 xl:space-x-20 md:left-30 md:right-30 sm:left-20 sm:right-20 xs:left-10 xs:right-10 xs:px-6 bg-slate-50"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ type: "spring", stiffness: 80, duration: 10, delay: 0.5 }}
+      >
+        <Link
+          to="/"
+          className="flex items-center"
+          onClick={() => {
+            window.scrollTo(0, 0);
+            setActive("");
+          }}
+        >
+          <motion.p id='nav-sohel' className="text-[18px] font-extrabold font-diphylleia bg-gradient-to-r from-violet-500 to-fuchsia-500 text-transparent bg-clip-text"
+            whileHover={{ scale: 1.14, transition: { duration: 0.3 } }}
+            variants={fadeIn("right", "spring", 1, 2.5)}
+            initial="hidden"
+            animate="show"
           >
-            <p id='nav-sohel' className="text-[18px] font-extrabold font-diphylleia bg-gradient-to-r from-violet-500 to-fuchsia-500 text-transparent bg-clip-text">
-              Sohel
-            </p>
-          </Link>
-          <ul className="list-none hidden justify-between gap-4 sm:flex">
+            Sohel
+          </motion.p>
+        </Link>
+        <motion.ul className="list-none hidden justify-between gap-4 sm:flex"
+          initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ type: "spring", bounce: 0.6, duration: 1.8, delay: 1.5 }}>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title ? "text-green-600" : "text-black-100"
+                } cursor-pointer font-sans text-[18px] hover:text-green-600 hover:scale-110`}
+              onClick={() => {
+                setActive(link.title);
+              }}
+            >
+              <a href={`${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+          <li className={`${active === 'resume' ? 'text-green-600' : 'text-black-100'} cursor-pointer hover:text-green-600 hover:scale-110 text-[18px]`}>
+            <a href={Resume} target="_blank" rel="noopener noreferrer" title="Sohel Resume">
+              resume
+            </a>
+          </li>
+        </motion.ul>
+        <motion.button
+          initial={{ rotate: 0 }}
+          animate={{ rotate: 180 }}
+          transition={{ type: "keyframes", duration: 1, delay: 1}}
+          className="sm:hidden md:hidden lg:hidden group relative"
+          onClick={() => setToggle(!toggle)}
+        >
+          <div
+            className={`${toggle && "-rotate-[45deg] -translate-y-[1px]"
+              } flex h-[20px] w-[20px] cursor-pointer origin-center transform flex-col justify-between transition-all duration-300`}
+          >
+            <div
+              className={`${toggle && "-rotate-90"
+                } h-[2px] w-1/2 origin-right transform rounded bg-purple-500 transition-all delay-75 duration-300 group-focus:h-[1px]`}
+            ></div>
+            <div
+              className={`${toggle ? "h-[2px]" : "h-[1px]"
+                } rounded bg-purple-500`}
+            ></div>
+            <div
+              className={`${toggle && "-rotate-90 translate-y-[1px]"
+                } h-[2px] w-1/2 origin-left transform self-end rounded bg-purple-500 transition-all delay-75 duration-300`}
+            ></div>
+          </div>
+        </motion.button>
+        <div
+          className={`${toggle ? "flex slide-down" : "hidden"
+            } absolute top-[65px] right-0 z-10 border bg-white w-full h-auto rounded-2xl flex-col sm:hidden`}
+        >
+          <ul className={`list-none flex flex-col justify-between gap-4 px-4 py-3`}>
             {navLinks.map((link) => (
               <li
                 key={link.id}
                 className={`${active === link.title ? "text-green-600" : "text-black-100"
-                  } cursor-pointer font-sans text-[18px] hover:text-green-600 hover:scale-110`}
+                  } cursor-pointer font-sans text-[18px] hover:text-green-600`}
                 onClick={() => {
                   setActive(link.title);
                 }}
@@ -41,54 +101,9 @@ const Navbar = () => {
               </a>
             </li>
           </ul>
-          <button
-            className="sm:hidden md:hidden lg:hidden group relative"
-            onClick={() => setToggle(!toggle)}
-          >
-            <div
-              className={`${toggle && "-rotate-[45deg] -translate-y-[1px]"
-                } flex h-[20px] w-[20px] cursor-pointer origin-center transform flex-col justify-between transition-all duration-300`}
-            >
-              <div
-                className={`${toggle && "-rotate-90"
-                  } h-[2px] w-1/2 origin-right transform rounded bg-purple-500 transition-all delay-75 duration-300 group-focus:h-[1px]`}
-              ></div>
-              <div
-                className={`${toggle ? "h-[2px]" : "h-[1px]"
-                  } rounded bg-purple-500`}
-              ></div>
-              <div
-                className={`${toggle && "-rotate-90 translate-y-[1px]"
-                  } h-[2px] w-1/2 origin-left transform self-end rounded bg-purple-500 transition-all delay-75 duration-300`}
-              ></div>
-            </div>
-          </button>
-          <div
-            className={`${toggle ? "flex slide-down" : "hidden"
-              } absolute top-[65px] right-0 z-10 border bg-white w-full h-auto rounded-2xl flex-col sm:hidden`}
-          >
-            <ul className={`list-none flex flex-col justify-between gap-4 px-4 py-3`}>
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${active === link.title ? "text-green-600" : "text-black-100"
-                    } cursor-pointer font-sans text-[18px] hover:text-green-600`}
-                  onClick={() => {
-                    setActive(link.title);
-                  }}
-                >
-                  <a href={`${link.id}`}>{link.title}</a>
-                </li>
-              ))}
-              <li className={`${active === 'resume' ? 'text-green-600' : 'text-black-100'} cursor-pointer hover:text-green-600 hover:scale-110 text-[18px]`}>
-                <a href={Resume} target="_blank" rel="noopener noreferrer" title="Sohel Resume">
-                  resume
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
-      </nav>
+      </motion.div>
+    </nav>
   );
 };
 
