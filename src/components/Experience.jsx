0fc025e7e75df10501e3e1846fa3a67feb9d4774
sohelkh1211 <khansohailabdussalam207@gmail.com>
2024-02-02 00:01:55 +0530

    Added motion to Experience.jsx but remaining for TimeLine comp.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { motion } from 'framer-motion'
+import { useState, useEffect, useRef } from "react";
+import { motion, useInView } from 'framer-motion'
 import { Tilt } from 'react-tilt'
 import { experience } from "../constants";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
@@ -69,6 +69,24 @@ const Experience = () => {
         window.innerWidth,
         window.innerHeight,
     ]);
+    const [view, setView] = useState(false);
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 200 && window.scrollY < 800) {
+                setView(true);
+            }
+            else {
+                setView(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
 
     useEffect(() => {
         const handleWindowResize = () => {
@@ -86,29 +104,47 @@ const Experience = () => {
     return (
         <>
             <TabTitle newtitle="Sohel Khan | Portfolio" />
-            <div className="absolute flex flex-col sm:flex top-[550px] lg:mt-6 sm:left-[40%] xs:left-[23%] items-center mx-auto">
+            <div className="absolute flex flex-col sm:flex lg:top-[520px] xs:top-[550px] lg:mt-6 sm:left-[40%] xs:left-[23%] items-center mx-auto">
                 <Tilt>
-                    <motion.div className="flex flex-col m-4 p-6 w-40 h-40 rounded-xl fun-gradient1"
-                        initial={{ pathLength: 0, opacity: 0}} 
-                        animate={{ pathLength: 1, opacity: 1 }}
-                        // onScroll={{ pathLength: 1, opacity: 1}}
+                    {/* Specify view only for animate  */}
+                    {/* initial means origin of animation and animate means the end position of animation */}
+                    {/* Note :- view ? value1: value2. Here, the value2 should be the value of initial. E.g :- value of opacity in initial is 0. Therefore, in animate value2 of opacity should be 0. */}
+                    <motion.div ref={ref} className="flex flex-col m-4 p-6 w-40 h-40 rounded-xl fun-gradient1"
+                        initial={{ opacity: 0, translateX: -1000 }}
+                        animate={{ opacity: view ? 1 : 0, translateX: view ? 0 : -1000 }}
                         transition={{
-                            default: { duration: 4, ease: "easeInOut" },
-                            fill: { duration: 4, ease: [1, 0, 0.8, 1] }
+                            type: "spring",
+                            duration: 4,
+                            delay: 0
                         }}
                     >
-                        <div className="container flex m-0.8 w-11 h-11 items-center justify-center mx-auto">
+                        <motion.div className="container flex m-0.8 w-11 h-11 items-center justify-center mx-auto"
+                            initial={{ scale: 0, opacity: 0 }}
+                            animate={{ scale: view ? 1 : 0, opacity: view ? 1 : 0 }}
+                            transition={{
+                                type: "spring",
+                                bounce: 0.4,
+                                duration: 3,
+                                delay: 0.6,
+                                ease: "easeOut"
+                            }}
+                        >
                             <h1 className="font-bold text-[40px] stylish_number">04</h1>
-                        </div>
-                        <div className="container flex w-22 h-18 mx-auto mt-7">
+                        </motion.div>
+                        <motion.div initial={{ opacity: 0, rotate: 0 }} animate={{ opacity: view ? 1 : 0, rotate: view ? 360 : 0, }} transition={{
+                            type: "spring",
+                            duration: view ? 2 : 0,
+                            delay: view ? 0.8 : 0
+                        }} className="container flex w-22 h-18 mx-auto mt-7">
                             <p className="font-bold text-[13px] text-white">Years of learning Experience</p>
-                        </div>
+                        </motion.div>
                     </motion.div>
+
                 </Tilt>
             </div>
-            <div className="container absolute top-[780px] mx-auto xs:left-[0.1%] xs:right-[1%] flex flex-row items-center justify-center">
+            <motion.div initial={{ opacity: 0, translateY: -500 }} animate={{ opacity: view ? 1 : 0, translateY: view ? 0 : -500 }} transition={{ type: "spring", duration: view ? 2 : 0, delay: view ? 0.8 : 0 }} className="container absolute top-[780px] mx-auto xs:left-[0.1%] xs:right-[1%] flex flex-row items-center justify-center">
                 <p className="lg:text-[18px] md:sm:text-[15px] xs:text-[14px] font-semibold md:max-w-[600px] sm:max-w-[500px] xs:max-w-[550px]">During this 4 years of learning I've acquired comprehensive set of skills and knowledge. I have honed my ability to analyze complex data sets, develop predictive models, and deploy them effectively & I also developed strong problem-solving skills. </p>
-            </div>
+            </motion.div>
             <div className="container relative top-[950px] xs:right-[60px] sm:right-[15px] flex flex-col mx-auto">
                 <VerticalTimeline layout="2-columns" lineColor="linear-gradient(to bottom, #60A5FA 5%, #3B0764 , #38A169 50%, #9F7AEA)">
                     {experience.map((experience, index) => (
